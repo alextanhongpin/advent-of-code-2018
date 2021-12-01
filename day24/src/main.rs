55903fbd640340fab3b9a40761eef3b714619d1b
alextanhongpin <alextan220990@gmail.com>
2021-12-01 21:18:16 +0800

    chore: day24, part 1, wip

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,7 +1,12 @@
 use regex::Regex;
 use std::collections::{HashMap, HashSet};
 
-fn main() {}
+fn main() {
+    let input = include_str!("input.txt");
+    let mut system = System::new(input);
+    // 18221 - 18250
+    assert_eq!(0, system.fight());
+}
 
 #[derive(Debug, Clone, Hash, Eq, PartialEq)]
 struct Group {
@@ -21,33 +26,24 @@ impl Group {
         self.attack_damage * self.units
     }
 
-    fn calculate_damage(&self, other: &Group) -> i32 {
+    fn effectiveness(&self, other: &Group) -> i32 {
         if other.immunities.contains(&self.attack_type) {
             0
         } else if other.weaknesses.contains(&self.attack_type) {
-            self.effective_power() * 2
+            2
         } else {
-            self.effective_power()
+            1
         }
     }
 
-    fn damage(&mut self, other: &mut Group) {
+    fn calculate_damage(&self, other: &Group) -> i32 {
+        self.effective_power() * self.effectiveness(other)
+    }
+
+    fn attack(&mut self, other: &mut Group) {
         let damage = self.calculate_damage(other);
-        let groups = damage / other.hit_points;
-        println!(
-            "type({}: {}) deals {} {} damage to type({}: {}) {} groups (HP: {}) with weaknesses({:?}) immunities ({:?})",
-            self.id,
-            self.is_immune,
-            damage,
-            self.attack_type,
-            other.id,
-            other.is_immune,
-            groups,
-            other.hit_points,
-            other.weaknesses,
-            other.immunities
-        );
-        other.units -= groups;
+        let units = damage / other.hit_points;
+        other.units = (other.units - units).max(0);
     }
 }
 
@@ -68,77 +64,69 @@ impl System {
         }
 
         loop {
-            let has_attacker = unit_by_id.iter().any(|(_, group)| group.is_immune);
-            let has_defender = unit_by_id.iter().any(|(_, group)| !group.is_immune);
-            if has_attacker != has_defender {
-                println!(
-                    "{:?}",
-                    unit_by_id
-                        .iter()
-                        .map(|(_, group)| group.units)
-                        .collect::<Vec<_>>()
-                );
-                return unit_by_id.iter().map(|(_, group)| group.units).sum::<i32>();
+            let (immune_units, infection_units) =
+                unit_by_id.iter().fold((0, 0), |groups, (_, group)| {
+                    if group.is_immune {
+                        (groups.0 + group.units, groups.1)
+                    } else {
+                        (groups.0, groups.1 + group.units)
+                    }
+                });
+            if immune_units == 0 || infection_units == 0 {
+                return immune_units.max(infection_units);
             }
-            let mut targets = self.target_selection();
-            targets.sort_by_key(|(atk, _)| match unit_by_id.get(atk) {
-                Some(group) => -group.initiative,
-                None => 0,
-            });
+            let mut targets =
+                self.target_selection(unit_by_id.values().cloned().collect::<Vec<Group>>());
+            targets.sort_by_key(|(id, _)| -unit_by_id[id].initiative);
             for (attacker, defender) in targets.iter_mut() {
-                //println!("{} attacks {:?}", attacker, unit_by_id.get(&attacker));
                 if !unit_by_id.contains_key(attacker) || !unit_by_id.contains_key(defender) {
                     continue;
                 }
                 let mut attacker = unit_by_id[attacker].clone();
                 let mut defender = unit_by_id[defender].clone();
-                if attacker.units == 0 {
+                if attacker.units <= 0 {
                     unit_by_id.remove(&attacker.id);
                     continue;
                 }
-                if defender.units == 0 {
+                if defender.units <= 0 {
                     unit_by_id.remove(&defender.id);
                     continue;
                 }
-
-                attacker.damage(&mut defender);
+                attacker.attack(&mut defender);
                 if defender.units <= 0 {
                     unit_by_id.remove(&defender.id);
                 } else {
                     unit_by_id.insert(defender.id, defender);
                 }
             }
-            println!();
         }
     }
 
-    fn target_selection(&mut self) -> Vec<(i32, i32)> {
+    fn target_selection(&self, groups: Vec<Group>) -> Vec<(i32, i32)> {
         let mut targets = Vec::new();
-        let mut targetted = HashSet::new();
-        self.groups
-            .sort_by_key(|u| (-u.effective_power(), -u.initiative));
-        for atk in &self.groups {
-            let defenders = self
-                .groups
+        let mut targeted = HashSet::new();
+
+        let mut groups = groups;
+        groups.sort_by_key(|u| (-u.effective_power(), -u.initiative));
+        for atk in groups.iter() {
+            let defender = groups
                 .clone()
                 .into_iter()
-                .filter(|def| def.is_immune != atk.is_immune && !targetted.contains(&def.id))
-                .collect::<Vec<Group>>();
-            let defender = defenders.iter().max_by_key(|def| {
-                (
-                    atk.calculate_damage(&def),
-                    def.effective_power(),
-                    def.initiative,
-                )
-            });
-            match defender {
-                Some(def) => {
-                    targetted.insert(def.id);
-                    targets.push((atk.id, def.id));
-                }
-                None => {
-                    continue;
-                }
+                .filter(|def| {
+                    def.is_immune != atk.is_immune
+                        && !targeted.contains(&def.id)
+                        && atk.calculate_damage(def) > 0
+                })
+                .max_by_key(|def| {
+                    (
+                        atk.calculate_damage(def),
+                        def.effective_power(),
+                        def.initiative,
+                    )
+                });
+            if let Some(def) = defender {
+                targets.push((atk.id, def.id));
+                targeted.insert(def.id);
             }
         }
         targets
@@ -146,7 +134,7 @@ impl System {
 }
 
 fn parse(input: &str) -> Vec<Group> {
-    let units_re = Regex::new(r"^(\d+) groups each with (\d+) hit points").unwrap();
+    let units_re = Regex::new(r"^(\d+) units each with (\d+) hit points").unwrap();
     let attack_re =
         Regex::new(r"with an attack that does (\d+) (\w+) damage at initiative (\d+)").unwrap();
     let immune_re = Regex::new(r"\((.+?)\)").unwrap();
@@ -185,18 +173,19 @@ fn parse(input: &str) -> Vec<Group> {
         group.units = units;
         group.hit_points = hit_points;
 
-        match attack_re.captures(line) {
-            Some(attack_cap) => {
-                group.attack_damage = attack_cap[1].parse::<i32>().unwrap();
-                group.attack_type = attack_cap[2].to_string();
-                group.initiative = attack_cap[3].parse::<i32>().unwrap();
-            }
-            None => (),
+        if let Some(attack_cap) = attack_re.captures(line) {
+            group.attack_damage = attack_cap[1].parse::<i32>().unwrap();
+            group.attack_type = attack_cap[2].to_string();
+            group.initiative = attack_cap[3].parse::<i32>().unwrap();
         }
 
-        let immune_cap = immune_re.captures(line).unwrap();
+        let immune_cap = immune_re.captures(line);
+        if immune_cap.is_none() {
+            continue;
+        }
+        let immune_cap = immune_cap.unwrap();
         let mut parts = immune_cap[1].split(';').map(str::trim).collect::<Vec<_>>();
-        parts.sort();
+        parts.sort_unstable();
         if parts.len() == 1 {
             if parts[0].starts_with("weak to") {
                 let weaknesses = parts[0]
@@ -252,12 +241,12 @@ mod tests {
     #[test]
     fn it_works() {
         let input = "Immune System:
-17 groups each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2
-989 groups each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
+17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2
+989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3
 
 Infection:
-801 groups each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1
-4485 groups each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4";
+801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1
+4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4";
 
         let mut system = System::new(input);
         assert_eq!(5216, system.fight());
